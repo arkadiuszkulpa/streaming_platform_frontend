a83a8117daccce37aa3a8dbc5e2afeb0ecfeb196
Arkadiusz Kulpa <arkadiusz.kulpa.91@gmail.com>
2025-05-05 19:09:32 +0100

    fixing deployment build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), createHtmlPlugin({
+    inject: {
+      data: {
+        title: 'MyStream.AI | Movie Picks You’ll Actually Love',
+      },
+    },
+  })],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    },
   },
   test: {
     globals: true,
